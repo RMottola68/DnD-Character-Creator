4e1b2dcf938b2bf9bb549d4094e792e956d2122e
Braylan Dodson <braylan1005@gmail.com>
2022-07-12 17:23:25 -0400

    getSpells

--- src/SpellSearch.js
+++ src/SpellSearch.js
@@ -1,7 +1,24 @@
+import { Container, Row, Col, Form, Button } from 'react-bootstrap';
+import {useState, useEffect} from "react";
 function SpellSearch(){
+    const [spells, setSpells] = useState([]);
+    function getSpells(){
+        fetch(`https://api.open5e.com/spells/?limit=321`)
+        .then((res)=>res.json())
+        .then((spellData)=> setSpells(spellData))
+    }
+    
+    useEffect(getSpells,[])
+    const spellList = spells.map((spell, index)=>{return(
+        
+    )})
     return(
-        <div>penis</div>
+        <div>penis
+            <Button className="m-3" variant="danger" onClick={getSpells}>Get spells</Button>
+        </div>
+        
     )
+    
 }
 
 export default SpellSearch;
